9f5771802bbd3459c050f4d36dd47be3388882c5
Zhanet <zhanet_f@mail.ru>
2023-01-16 19:59:26 +0300

    create react app

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Setting from "./Setting";
+import Counter from "./Counter";
+import {logDOM} from "@testing-library/react";
 
 function App() {
-  return (
-    <div className="App">
+    const defaultStart: number = 0
+    const defaultMax: number = 0
+    const [value, setValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(defaultMax)
+    const [startValue, setStartValue] = useState<number>(defaultStart)
 
-    </div>
-  );
+    const incValue = () => {
+        if (value < maxValue) {
+            setValue(value + 1)
+        }
+    }
+    const resetValue = () => {
+        setValue(startValue)
+    }
+
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('MaxValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setMaxValue(newValue)
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('MaxValue', JSON.stringify(maxValue))
+    }, [maxValue])
+
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('StartValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setStartValue(newValue)
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('StartValue', JSON.stringify(startValue))
+    }, [startValue])
+
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('currentValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('currentValue', JSON.stringify(startValue))
+    }, [startValue])
+
+    return (
+        <div className="App">
+            <Counter
+                value={startValue}
+                incValue={incValue}
+                resetValue={resetValue}
+            />
+            <Setting
+
+                maxValue={maxValue}
+                startValue={startValue}
+                setMaxValue={setMaxValue}
+                setStartValue={setStartValue}
+
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ counter/src/Counter.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type CounterType = {
+    value: number
+    incValue: () => void
+    resetValue: () => void
+}
+
+const Counter = (props: CounterType) => {
+    return (
+        <div>
+            <div>{props.value}</div>
+            <div>
+                <button onClick={props.incValue}>Inc</button>
+                <button onClick={props.resetValue}>Reset</button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Counter;
